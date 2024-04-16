c200508001c5dcc3141e00dffa0a08fc500b1446
AshikJenly <jenlyashik@gmail.com>
2024-04-16 23:08:00 +0530

    added EX10

---
+++ EX_10.r
@@ -0,0 +1,25 @@
+library(ggplot2)
+
+
+statistic <- function(s,p)
+{
+  n_m = mean(s) - mean(p)
+  d_m = sd(s) / sqrt(length(s))
+  return(n_m/d_m)
+}
+
+precipitation <- rnorm(100000,mean=38)
+stats <- numeric(10000)
+
+for(i in 1:10000)
+{
+  stats[i] <- statistic(sample(precipitation,70),precipitation)
+}
+stats
+qplot(x = stats,geom="histogram")
+
+
+statistic(precip,precipitation)
+
+t.test(precip, mu=38)
+t.test(precip, mu=38, alternative="less")
